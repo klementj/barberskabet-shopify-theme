a35451651f63249565bbf78697e5b782fefb5974
klementj <klementj@gmail.com>
2020-01-09 17:39:50 +0100

    :bug: Add correct decimal point for DA currency

--- src/scripts/templates/cart.js
+++ src/scripts/templates/cart.js
@@ -7,34 +7,34 @@ $('input').change(function() {
   const maxValue = parseInt($(this).attr('max'), 10);
   const key = $(this).attr('id').split('_', 2)[1];
 
-  console.log(`Input for ${key}`);
+  // console.log(`Input for ${key}`);
 
   if (currentQuantity === minValue) {
     cart.removeItem(key).then(state => {
-      console.log('This item was removed');
+      // console.log('This item was removed');
       updateCartTotals($(this), currentQuantity, 0, state.total_price);
       removeCartRow($(this));
     });
   } else if (!maxValue) {
     cart.updateItem(key, {quantity: currentQuantity}).then(state => {
       const currentItem = state.items.find(item => item.key === key);
-      console.log(`The item with key '${key}' now has quantity ${currentItem.quantity} and a price of ${currentItem.price}`);
+      // console.log(`The item with key '${key}' now has quantity ${currentItem.quantity} and a price of ${currentItem.price}`);
       updateCartTotals($(this), currentItem.quantity, currentItem.price, state.total_price);
     });
   } else if (currentQuantity <= maxValue) {
     cart.updateItem(key, {quantity: currentQuantity}).then(state => {
       const currentItem = state.items.find(item => item.key === key);
-      console.log(`The item with key '${key}' now has quantity ${currentItem.quantity}`);
+      // console.log(`The item with key '${key}' now has quantity ${currentItem.quantity}`);
       updateCartTotals($(this), currentItem.quantity, currentItem.price, state.total_price);
     });
   }
 });
 
 function updateCartTotals(thisObj, itemPrice, quantity, subtotal) {
-  const itemTotal = ((itemPrice * quantity) / 100).toFixed(2);
+  const itemTotal = ((itemPrice * quantity) / 100).toFixed(2).replace(".", ",");
   thisObj.parents().eq(1).children("td.cart-money[data-label='Total']")
-    .text(`${itemTotal}`);
-  $('#subtotal').text(`${(subtotal / 100).toFixed(2)} kr`);
+    .text(`${itemTotal} kr`);
+  $('#subtotal').text(`${(subtotal / 100).toFixed(2).replace(".", ",")} kr`);
 }
 
 function removeCartRow(thisObj) {
